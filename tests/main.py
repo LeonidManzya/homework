d616190d2b4f19a8cf9f16056eba990230d6986e
Leonid <leny340@gmail.com>
2025-03-18 15:27:10 +0300

    home_work 9.1

---
+++ tests/main.py
@@ -0,0 +1,5 @@
+from src.masks import get_mask_account_number
+from src.masks import get_mask_card_number
+
+print(get_mask_card_number("9485213408774653"))
+print(get_mask_account_number("84638237493322774455"))
