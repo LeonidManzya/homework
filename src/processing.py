66865661b1b245cc2415346efab9c1fbc935a83d
Leonid <leny340@gmail.com>
2025-03-28 02:03:09 +0300

    home_work 10.1 processing.py part 2

---
+++ src/processing.py
@@ -0,0 +1,11 @@
+def filter_by_state(opiration: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """Функция возвращает новый список словарей, содержащий только те словари,
+     у которых ключ state соответствует указанному значению."""
+
+    return [item for item in opiration if item.get('state') == state]
+
+
+def sort_be_date(operation: list[dict], flow: bool = True) -> list[dict]:
+    """Функция возвращает новый список, отсортированный по дате."""
+
+    return sorted(operation, key = lambda item : item['date'], reverse = flow)
--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,4 @@
-from src.masks import get_mask_account_number
-from src.masks import get_mask_card_number
+from src.masks import get_mask_account_number, get_mask_card_number
 
 
 def mask_account_card(account_card: str) -> str | None:
@@ -19,19 +18,6 @@ def mask_account_card(account_card: str) -> str | None:
             return f"Счет {get_mask_account_number(''.join(number))}"
 
 
-print(mask_account_card("Maestro 1596837868705199"))
-print(mask_account_card("Счет 64686473678894779589"))
-print(mask_account_card("MasterCard 7158300734726758"))
-print(mask_account_card("Счет 35383033474447895560"))
-print(mask_account_card("Visa Classic 6831982476737658"))
-print(mask_account_card("Visa Platinum 8990922113665229"))
-print(mask_account_card("Visa Gold 5999414228426353"))
-print(mask_account_card("Счет 73654108430135874305"))
-
-
 def get_date(date_time: str) -> str:
     """Функция для преобразования даты и времени в формат ДД.ММ.ГГГГ"""
     return f"{date_time[8:10]}.{date_time[5:7]}.{date_time[:4]}"
-
-
-print(get_date("2024-03-11T02:26:18.671407"))
