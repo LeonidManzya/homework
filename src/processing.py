32897a448ecc2b6bc4ad00cf0e4f4f5e2709cdfe
Leonid <leny340@gmail.com>
2025-03-29 07:41:10 +0300

    home_work 10.1 corrected

--- src/processing.py
+++ src/processing.py
@@ -5,7 +5,7 @@ def filter_by_state(opiration: list[dict], state: str = "EXECUTED") -> list[dict
     return [item for item in opiration if item.get("state") == state]
 
 
-def sort_be_date(operation: list[dict], flow: bool = True) -> list[dict]:
+def sort_by_date(operation: list[dict], flow: bool = True) -> list[dict]:
     """Функция возвращает новый список, отсортированный по дате."""
 
     return sorted(operation, key=lambda item: item["date"], reverse=flow)
