b8f3d0065c199620d2594d8197df0f84d82f042d
Leonid <leny340@gmail.com>
2025-03-29 07:48:15 +0300

    home_work 10.1 corrected 2

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,8 @@
-def filter_by_state(opiration: list[dict], state: str = "EXECUTED") -> list[dict]:
+def filter_by_state(operation: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция возвращает новый список словарей, содержащий только те словари,
     у которых ключ state соответствует указанному значению."""
 
-    return [item for item in opiration if item.get("state") == state]
+    return [item for item in operation if item.get("state") == state]
 
 
 def sort_by_date(operation: list[dict], flow: bool = True) -> list[dict]:
