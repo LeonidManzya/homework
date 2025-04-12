db93ccc54b94bb3fb13428fedb9583bef4341f90
Leonid <leny340@gmail.com>
2025-04-13 02:05:44 +0300

    home_work 10.2 end

--- tests/main.py
+++ tests/main.py
@@ -1,8 +1,10 @@
-#from src.masks import get_mask_account_number
+# from src.masks import get_mask_account_number
 from src.masks import get_mask_card_number
+from src.processing import filter_by_state
+from src.processing import sort_by_date
 
-#print(get_mask_card_number("9485213408774653"))
-#print(get_mask_account_number("84638237493322774455"))
+# print(get_mask_card_number("9485213408774653"))
+# print(get_mask_account_number("84638237493322774455"))
 
 # from src.widget import get_date
 # from src.widget import mask_account_card
@@ -16,18 +18,26 @@ from src.masks import get_mask_card_number
 # print(mask_account_card("Visa Gold 5999414228426353"))
 # print(mask_account_card("Счет 73654108430135874305"))
 
-# print(get_date("2024-03-11T02:26:18.671407"))
+# print(get_date("2024-33-11T02:26:18.671407"))
 
-#from src.processing import filter_by_state, sort_by_date
 
-#print(
- #   filter_by_state([
- #           {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
- #           {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
- #           {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
- #           {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},]))
-#print(sort_by_date([
- #           {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
- #           {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
- #           {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-  #          {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},]))
+print(
+    filter_by_state(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)
+print(
+    sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)
